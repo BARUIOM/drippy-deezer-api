03a8c28e280bb051b2713a8ad4501b6b1ce661ae
VitorMac10 <vitormac3@hotmail.com>
2020-04-30 17:33:48 -0300

    Deezer Web API authentication strategy

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const axios = require('axios').default;
+const Axios = require('axios').default;
+const axios = Axios.create();
+
 const web_api = 'https://www.deezer.com/ajax/gw-light.php';
 const rest_api = 'https://api.deezer.com';
 
@@ -7,27 +9,42 @@ const Track = require('./model/dz-track.js');
 module.exports = class Deezer {
 
     constructor(arl_token) {
-        this.session = `arl=${arl_token}; `;
-        axios.get(web_api, {
-            params: {
-                input: '3',
-                api_version: '1.0',
-                api_token: '',
-                method: 'deezer.getUserData'
-            },
-            headers: { 'Cookie': this.session }
-        }).then(response => {
-            this.api_token = response.data.results['checkForm'];
-            this.session += `sid=${response.data.results['SESSION_ID']}`;
+        this.cookies = [`arl=${arl_token}`];
+        axios.interceptors.request.use(request => {
+            request.headers['Cookie'] = this.cookies.join('; ');
+
+            if (request.url === web_api) {
+                request.params['input'] = '3';
+                request.params['api_version'] = '1.0';
+                request.params['api_token'] = this.api_token || '';
+            }
+            return request;
         });
+
+        this.authenticate();
+        axios.interceptors.response.use(response => {
+            if (response.data['error'] && response.data['error']['VALID_TOKEN_REQUIRED']) {
+                delete this.api_token;
+                this.cookies.splice(1, 1);
+                return this.authenticate().then(() => axios.request(response.config));
+            }
+            return response;
+        });
+    }
+
+    async authenticate() {
+        const response = await axios.get(web_api, {
+            params: { method: 'deezer.getUserData' }
+        });
+        this.api_token = response.data.results['checkForm'];
+        this.cookies[1] = `sid=${response.data.results['SESSION_ID']}`;
     }
 
     async search(query, track) {
         if (track !== undefined) {
             const res = await axios.get('/search/track', {
                 baseURL: rest_api,
-                params: { q: `artist:"${query}" track:"${track}"` },
-                headers: { 'Cookie': this.session }
+                params: { q: `artist:"${query}" track:"${track}"` }
             });
 
             return [...res.data['data']].map(e => new Track(e));
@@ -35,26 +52,14 @@ module.exports = class Deezer {
 
         const data = { query: query, filter: '', output: 'TRACK' };
         const res = await axios.post(web_api, data, {
-            params: {
-                input: '3',
-                api_version: '1.0',
-                api_token: this.api_token,
-                method: 'search.music'
-            },
-            headers: { 'Cookie': this.session }
+            params: { method: 'search.music' }
         });
         return [...res.data.results['data']].map(e => new Track(e));
     }
 
     async getTrack(track_id) {
         const res = await axios.post(web_api, { sng_id: track_id }, {
-            params: {
-                input: '3',
-                api_version: '1.0',
-                api_token: this.api_token,
-                method: 'song.getData'
-            },
-            headers: { 'Cookie': this.session }
+            params: { method: 'song.getData' }
         });
         return res.data['results'];
     }
